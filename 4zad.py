26a2093bd170698d1ae42bb05d66286f292fd63c
maximim10 <64524141+maximim10@users.noreply.github.com>
2020-05-06 10:43:21 +0300

    1

---
+++ 4zad.py
@@ -0,0 +1,48 @@
+from PIL import Image, ImageDraw
+N1=1
+N2=255
+N=3
+inp=input().split()
+try:
+    N1=int(inp[0])
+except:
+    N1=1
+try:
+    N2=int(inp[1])
+except:
+    N2=255
+try:
+    N=int(inp[2])
+except:
+    N=14
+try:
+    image_inp=inp[3]
+except:
+    image_inp="kachok.jpg"
+try:
+    image_out=inp[4]
+except:
+    image_out="out.jpg"
+try:
+    image=Image.open(image_inp)
+except:
+    print ("Net Faila")
+else:
+    draw=ImageDraw.Draw(image)
+    pix=image.load()
+
+    """N1=int(input())
+    N2=int(input())
+    N=int(input())
+    inp=input()
+    out=input()"""
+    for i in range (image.size[0]):
+        for j in range (image.size[1]):
+            a=pix[i,j][0]
+            b=pix[i,j][1]
+            c=pix[i,j][2]
+            if a+b+c>=N1 and a+b+c<=N2:
+                draw.point((i,j),(N,N,N))
+    image.save(image_out,"jpeg")
+
+    image.show()
